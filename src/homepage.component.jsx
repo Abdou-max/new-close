b4fe384f50fcfa3b90c92d78df2aae21428c772c
Abdou-max <abdelwahed.dev@gmail.com>
2020-12-18 00:05:12 +0100

    initial commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './homepage.style.scss'
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>jaket</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>nothing</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>jaket3</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>womens</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>mens</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+           
+        </div>
+    </div>
+);
+export default HomePage;
